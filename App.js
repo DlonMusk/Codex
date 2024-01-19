06ec20a149fbd57bad04c68c65c3933247b4b35c
DlonMusk <knight777@hotmail.ca>
2024-01-19 05:02:37 -0500

    plllease

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { TailwindProvider } from 'tailwindcss-react-native';
 import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { getDoc, doc } from 'firebase/firestore';
 import BarsScreen from './screens/BarsScreen';
-import { db, auth } from './firebase.js'
+import { auth } from './firebase.js'
 import OverallScoresScreen from './screens/OverallScores.js';
 
 
@@ -19,7 +15,7 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState('')
 
 
   useEffect(() => {
@@ -36,31 +32,29 @@ export default function App() {
 
   return (
 
-    <TailwindProvider >
-      <NavigationContainer>
-        <Stack.Navigator>
-          {!user ? (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {!user ? (
+          <Stack.Screen
+            name='Login'
+            component={LoginScreen}
+          />
+        ) : (
+          <>
             <Stack.Screen
-              name='Login'
-              component={LoginScreen}
+              name='Bars'
+              component={BarsScreen}
+              initialParams={{ uid: user }}
             />
-          ) : (
-            <>
-              <Stack.Screen
-                name='Bars'
-                component={BarsScreen}
-                initialParams={{ uid: user }}
-              />
-              <Stack.Screen
-                name='Overall Scores'
-                component={OverallScoresScreen}
-                initialParams={{ uid: user }}
-              />
-            </>
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
-    </TailwindProvider>
+            <Stack.Screen
+              name='Overall Scores'
+              component={OverallScoresScreen}
+              initialParams={{ uid: user }}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
 
 
   );
